99224a7e3f13653e89dd2712006e38e884eb02a7
Rohan Chambers <rohanchambers888@gmail.com>
2016-07-05 15:22:58 +0100

    Home page responsive development

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(function($){
+		// Carousel Jssor
         var options = {
         	//$SlideHeight: 400,
         	$ArrowNavigatorOptions: true,
@@ -30,4 +31,26 @@ $(function($){
         $(window).bind("resize", ScaleSlider);
         $(window).bind("orientationchange", ScaleSlider);
         //responsive code end
+
+
+        // Bourbon Refill Accordion
+		$('.accordion-tabs-minimal').each(function(index) {
+			$(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+		});
+
+		$('.accordion-tabs-minimal').on('click', 'li > a.tab-link', function(event) {
+			if (!$(this).hasClass('is-active')) {
+				event.preventDefault();
+				var accordionTabs = $(this).closest('.accordion-tabs-minimal');
+				accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+				$(this).next().toggleClass('is-open').toggle();
+				accordionTabs.find('.is-active').removeClass('is-active');
+				$(this).addClass('is-active');
+			} else {
+				event.preventDefault();
+			}
+		});
+
+
 });
