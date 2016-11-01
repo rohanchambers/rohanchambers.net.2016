5fc3690638f680123a38f26f27c8666a10385ed9
Rohan Chambers <rohanchambers888@gmail.com>
2016-11-01 01:44:30 +0000

    factorise JS with namespacing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,64 @@
-// Detect browser width
-function getWidth() {
-	if (self.innerHeight) {
-		return self.innerWidth;
-	}
+var APP = {}
 
-	if (document.documentElement && document.documentElement.clientHeight) {
-		return document.documentElement.clientWidth;
-	}
+$(function(){
+	APP.nav();
+	APP.lightbox();
+	APP.caseStudy();
+	APP.animateTo();
+	APP.mobileLogic();
+	APP.accordion();
+	APP.plugins();
+		APP.jssor();
+		APP.isotope();
 
-	if (document.body) {
-		return document.body.clientWidth;
-	}
-}
 
-// Back to the top button
-$('#back-to-top').click( function(){
-    $('html, body').animate({ scrollTop: 0}, 1000);
-    return false;
 });
 
-$(function($){
-	// Portfolio active state for filter buttons
-	$('.filter-button-group a').click( function(){
-		$('.filter-button-group a').removeClass('active');
-		$(this).addClass('active');
-	});
+// Change nav state
+$(window).resize(function() {
+	APP.windowResize();
+});
 
+// On scroll
+$(document).on('scroll',function(){
+	APP.onScroll();	
+});
+
+
+APP.nav = function() {
 	// Main nav active states
 	$('#nav-main li').click( function(){
 		$('#nav-main li').removeClass('active');
 		$(this).addClass('active');
 	});
 
+    // Main nav scroll to sections
+    $('#nav-main ul li a, #logo a').click(function(e) {
+    	//e.preventDefault();
+        var section = $(this).attr('href').split('#')[1];
+        // Nav home link go to top
+    	if (section == 'home') {
+	        $('html, body').animate({
+	            scrollTop: 0
+	        }, 1000);
+	        return false;
+    	} else {
+	        $('html, body').animate({
+	            scrollTop: $('#' + section).offset().top -75
+	        }, 1000);
+	        return false;
+    	}
+    });
+
+	// Portfolio filter buttons active state for 
+	$('.filter-button-group a').click( function(){
+		$('.filter-button-group a').removeClass('active');
+		$(this).addClass('active');
+	});
+}
+
+
+APP.lightbox = function() {
 	// Lightbox
 	var lightboxbg = $('.lightbox-bg');
 	var speed = 500;
@@ -59,59 +86,36 @@ $(function($){
 	$('.lightbox-panel').bind('click', function(){
 		return false;
 	});
+}
 
-    // Load isotope only on portfolio apge
-	if ($('#page-portfolio').length > 0) {
-		// Init Isotope
-		var $grid = $('.grid').isotope({
-		  	// Options
-			itemSelector: '.grid-item',
-			percentPosition: true,
-			layoutMode: 'fitRows',
 
-			masonry: {
-			// use element for option
-			columnWidth: '.grid-item'
-			}
-
-		});
-
-		// Filter items on button click
-		$('.filter-button-group').on( 'click', 'a', function() {
-			var filterValue = $(this).attr('data-filter');
-			$grid.isotope({ filter: filterValue });
-		});
-	}
-
+APP.caseStudy = function() {
 	// Open all links in case study in new window
 	$("#case-study-content .targetBlank[href^='http://']").attr('target','_blank');
 
-    // Main nav scroll to sections
-    $('#nav-main ul li a').click(function(e) {
-    	//e.preventDefault();
-        var section = $(this).attr('href').split('#')[1];
-        // Nav home link go to top
-    	if (section == 'home') {
-	        $('html, body').animate({
-	            scrollTop: 0
-	        }, 1000);
-	        return false;
-    	} else {
-	        $('html, body').animate({
-	            scrollTop: $('#' + section).offset().top -60
-	        }, 1000);
-	        return false;
-    	}
-    });
+	// Fade in Case study images 
+	$('#case-study-img img').hide();
+	$('#case-study-img img').fadeIn();
+}
 
-    // Page links scroll to seections
-	// function goToByScroll(id){
-	// 	$('html, body').animate({scrollTop: $("#"+id).offset().top}, 1500);
-	// };    
 
-	// $('#page-home a').click( function(){
-	// 	goToByScroll();
-	// });
+APP.animateTo = function() {
+    // Home Page links scroll to section
+	function goToByScroll(id){
+		$('html, body').animate({scrollTop: $("#"+id).offset().top - 75}, 1500);
+	};    
+
+	$('#about a, #what-i-do a').click( function(e){
+		e.preventDefault();
+		var linkName = $(this).attr('href').replace('#', '');
+		goToByScroll(linkName);
+	});
+
+	// Back to top button
+	$('#back-to-top').click( function(){
+	    $('html, body').animate({ scrollTop: 0}, 1000);
+	    return false;
+	});
 
     // Intro scroll down and up
     $('.intro-scroll-arrow').click(function(e) {    	
@@ -128,46 +132,30 @@ $(function($){
 	        $(this).toggleClass('scroll-up');
     	}
     });
+}
 
-    // Load carousel only on homepage
-	if ($('#page-home').length > 0) {
-		// Init Carousel Jssor
-        var options = {
-        	$SlideHeight: 420,
-        	$ArrowNavigatorOptions: true,
-        	$AutoPlay: true,
-        	$Loop: 1,
-        	$PauseOnHover: 2,
-        	$FillMode: 2,
-	        $ArrowNavigatorOptions: {
-	            $Class: $JssorArrowNavigator$
-	        }
-    	};
 
-        var jssor_slider1 = new $JssorSlider$('carousel', options);
+APP.mobileLogic = function() {
+	// Trigger mobile open nav
+	$('.c-hamburger').click( function(){
+		// Toggle hamburger icon
+		$(this).toggleClass('is-active');
+		// Toggle main nav
+		$('#header-main').toggleClass('mobile');
+	});
 
-        //responsive code begin | you can remove responsive code if you don't want the slider scales
-        //while window resizing
-        function ScaleSlider() {
-	        var parentWidth = $('#carousel').parent().width();
+	// Mobile Nav open logic
+	$('#header-main ul li a').on( 'click', function(){
+		$('.c-hamburger').toggleClass('is-active');
+		$('#header-main.mobile').toggleClass('mobile');
+		// Add and remove active states
+		$('#nav-main ul li').removeClass('active');
+		$(this).parent().addClass('active');
+	});
+}
 
-	        if (parentWidth) {
-	            jssor_slider1.$ScaleWidth(parentWidth);
-	        } else {
-	        	window.setTimeout(ScaleSlider, 30);
-	        }
-	    }
 
-        //Scale slider after document ready
-		ScaleSlider();
-
-        //Scale slider while window load/resize/orientation change.
-        $(window).bind("load", ScaleSlider);
-        $(window).bind("resize", ScaleSlider);
-        $(window).bind("orientationchange", ScaleSlider);
-        //responsive code end
-    }
-
+APP.accordion = function() {
     // Bourbon Refill Accordion
 	$('.accordion-tabs-minimal').each(function(index) {
 		$(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
@@ -186,34 +174,86 @@ $(function($){
 			event.preventDefault();
 		}
 	});
+}
 
-	// Trigger mobile open nav
-	$('.c-hamburger').click( function(){
-		// Toggle hamburger icon
-		$(this).toggleClass('is-active');
-		// Toggle main nav
-		$('#header-main').toggleClass('mobile');
-	});
 
-	// Mobile Nav open logic
-	$('#header-main ul li a').on( 'click', function(){
-		$('.c-hamburger').toggleClass('is-active');
-		$('#header-main.mobile').toggleClass('mobile');
-		// Add and remove active states
-		$('#nav-main ul li').removeClass('active');
-		$(this).parent().addClass('active');
-	});
-});
+APP.plugins = function() {
 
-// On window resize back to desktop hide mobile nav
-$( window ).resize(function() {
+	APP.jssor = function() {
+	    // Load carousel only on homepage
+		if ($('#page-home').length > 0) {
+			// Init Carousel Jssor
+	        var options = {
+	        	$SlideHeight: 420,
+	        	$ArrowNavigatorOptions: true,
+	        	$AutoPlay: true,
+	        	$Loop: 1,
+	        	$PauseOnHover: 2,
+	        	$FillMode: 2,
+		        $ArrowNavigatorOptions: {
+		            $Class: $JssorArrowNavigator$
+		        }
+	    	};
+
+	        var jssor_slider1 = new $JssorSlider$('carousel', options);
+
+	        //responsive code begin | you can remove responsive code if you don't want the slider scales
+	        //while window resizing
+	        function ScaleSlider() {
+		        var parentWidth = $('#carousel').parent().width();
+
+		        if (parentWidth) {
+		            jssor_slider1.$ScaleWidth(parentWidth);
+		        } else {
+		        	window.setTimeout(ScaleSlider, 30);
+		        }
+		    }
+
+	        //Scale slider after document ready
+			ScaleSlider();
+
+	        //Scale slider while window load/resize/orientation change.
+	        $(window).bind("load", ScaleSlider);
+	        $(window).bind("resize", ScaleSlider);
+	        $(window).bind("orientationchange", ScaleSlider);
+	        //responsive code end
+	    }
+	}
+
+	APP.isotope = function() {
+	    // Load isotope only on portfolio apge
+		if ($('#page-portfolio').length > 0) {
+			// Init Isotope
+			var $grid = $('.grid').isotope({
+			  	// Options
+				itemSelector: '.grid-item',
+				percentPosition: true,
+				layoutMode: 'fitRows',
+
+				masonry: {
+				// use element for option
+				columnWidth: '.grid-item'
+				}
+			});
+
+			// Filter items on button click
+			$('.filter-button-group').on( 'click', 'a', function() {
+				var filterValue = $(this).attr('data-filter');
+				$grid.isotope({ filter: filterValue });
+			});
+		}
+	}
+}
+
+
+APP.windowResize = function() {
+	// On window resize back to desktop hide mobile nav
 	$('.c-hamburger').removeClass('is-active');
 	$('#header-main').removeClass('mobile');
-});
+}
 
-// Document on scroll change nav state
-$(document).on('scroll',function(){
 
+APP.onScroll = function() {
 	// Homepage up down arrow. If document pos is at the top switch this arrow down
 	if( $(document).scrollTop() == 0) {
 		$('.intro-scroll-arrow').removeClass('scroll-up');
@@ -225,16 +265,14 @@ $(document).on('scroll',function(){
 	}
 
 	// On homeapge only go back to top show hide mini to full desktop nav
-
-	    var homeSectionHeight = 70;
-	    if( $(document).scrollTop() > homeSectionHeight) {
-	            $('#back-to-top').fadeIn().addClass('buttonTopPulse');
-	            $('#header-main').addClass('mini');
-	            $('.carousel').addClass('mini');
-	        } else {
-	            $('#back-to-top').fadeOut().removeClass('buttonTopPulse');
-	            $('#header-main').removeClass('mini');
-	            $('.carousel').removeClass('mini');
-	    }
-
-});
+    var homeSectionHeight = 70;
+    if( $(document).scrollTop() > homeSectionHeight) {
+            $('#back-to-top').fadeIn().addClass('buttonTopPulse');
+            $('#header-main').addClass('mini');
+            $('.carousel').addClass('mini');
+        } else {
+            $('#back-to-top').fadeOut().removeClass('buttonTopPulse');
+            $('#header-main').removeClass('mini');
+            $('.carousel').removeClass('mini');
+    }
+}
